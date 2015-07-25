892a00c1bec55b45cbf9e3e07e4bdadfc24e58ad
Masaaki Morishita <ikasama3.0@gmail.com>
2015-07-25 10:14:30 +0900

    write test code

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 module.exports = function (str) {
-  var regexp = [
-    /^(\+|-)?([0-9]*\.)?[0-9]+(em|ex|ch|rem|vh|vw|vmin|vmax|px|mm|cm|in|pt|pc|%)$/i,
-    /^(\+|-)?(0*\.)?0+$/
-  ]
-  regexp.forEach(function (re) {
-    if (re.test(str)) return true
-  })
+  var length =  /^(\+|-)?([0-9]*\.)?[0-9]+(em|ex|ch|rem|vh|vw|vmin|vmax|px|mm|cm|in|pt|pc|%)$/i
+  var zero =  /^(\+|-)?(0*\.)?0+$/
 
-  return false
+  return length.test(str) || zero.test(str)
 }
